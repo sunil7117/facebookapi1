53e715fcda6c5327956c2dca5d12cd9a3c7f4d47
SRcpr <completefrontend2021@gmail.com>
2022-11-19 23:31:21 +0530

    12

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signin, signup } from "../controllers/authController.js";
+import { signin, signup, welcome } from "../controllers/authController.js";
 const router = express.Router();
 console.log("get");
 // singup a user
+router.get("/welcome", welcome);
 router.post("/signup", signup);
 // singin a user
 router.post("/signin", signin);
